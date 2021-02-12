9d238a760213f8dd6bfe5125a7de2de483e59754
alanjian85 <alanjian85@outlook.com>
2021-02-12 18:32:34 +0800

    Update impl.cpp

---
+++ deps/impl.cpp
@@ -0,0 +1,6 @@
+/*
+ * This source file will implement header-only libraries 
+ */
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,33 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stb_image.h>
 
 int main()
 {
 	glfwInit();
+
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	GLFWwindow *window = glfwCreateWindow(800, 600, "Test", nullptr, nullptr);
+	
 	glfwMakeContextCurrent(window);
 	gladLoadGL();
+
+	GLFWimage icon;
+	icon.pixels = stbi_load("assets/icon.png", &icon.width, &icon.height, nullptr, 4);
+	glfwSetWindowIcon(window, 1, &icon);
+
 	glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+	
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
+		
 		glClear(GL_COLOR_BUFFER_BIT);
+		
 		glfwSwapBuffers(window);
 	}
+	
 	glfwTerminate();
 	return 0;
 }
